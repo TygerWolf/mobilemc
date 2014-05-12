a87a1f63a5c6030ea860096f6ee37014557d0938
Sandeep Dhanjani <sandeepdhanjani@gmail.com>
2014-05-12 15:41:50 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Mobilemc::Application.config.secret_key_base = 'af4c2b2eaaecd6abeed1add0608c3bd94e465c0839a5db3093e9b1d4fbb82950479acaaee3a1abe69471135be6c5fb91ad00741ab88df5f4be19d166a9335c91'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Mobilemc::Application.config.session_store :cookie_store, key: '_mobilemc_session'
