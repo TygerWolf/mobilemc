8e464e9788adb7f8926d253fcab52349a416860d
Sandeep Dhanjani <sandeepdhanjani@gmail.com>
2014-05-12 16:03:37 -0400

    Add Tracks Controller

--- app/controllers/tracks_controller.rb
+++ app/controllers/tracks_controller.rb
@@ -1,2 +1,48 @@
 class TracksController < ApplicationController
+  def index
+    @tracks = Track.all
+  end
+
+  def new
+    @track = Track.new
+  end
+
+  def create
+    @track = Track.new(track_params)
+    if @track.save
+      redirect_to track_path
+    else
+      render :new
+    end
+  end
+
+  def show
+    @track = Track.find(params[:id])
+  end
+
+  def edit
+    @track = Track.find(params[:id])
+  end
+
+  def update
+    @track = Track.find(params[:id])
+    if @track.update_attributes(track_params)
+      redirect_to track_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @track = Track.find(params[:id])
+    @track.destroy
+    redirect_to tracks_path
+  end
+
+  private
+
+  def track_params
+    params.require(:track).permit(:title, :hook, :barindicator)
+  end
+
 end
