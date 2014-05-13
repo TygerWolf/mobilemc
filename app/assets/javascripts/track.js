d4bc1bd0f104533e6677a66e1bdf1f0cba668380
Ryan Edwards <ryanmedwards@hotmail.com>
2014-05-13 13:51:34 -0400

    Add Enter Enter listener on lyric form

--- app/assets/javascripts/track.js
+++ app/assets/javascripts/track.js
@@ -1,11 +1,13 @@
 $(document).ready(function(){
 
-  $(document).keypress(function(event) {
-    if(event.which == 13) {
-    event.preventDefault();
-    alert("Hello");
+var counter = 0
 
+  $('body').keypress(function(e){
+    if(e.which === 13){
+      counter+=1;
+      if (counter == 1) {setTimeout(function(){counter=0},500)}
+      else if (counter == 2){alert('twice')}
     }
-  });
+  });      
 
 });
